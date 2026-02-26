078099fc0a4891837d246f196c1200d6911dd3c0
edenmoore10 <edenrm@email.sc.edu>
2026-02-26 18:23:02 -0500

    fixed #76

---
+++ src/main/java/com/model/CommentTag.java
@@ -0,0 +1,7 @@
+package com.model;
+/**
+ * Enum representing the different tags that can be used on a comment
+ */
+public enum CommentTag {
+    QUESTION, SOLUTION, EDITORSOLUTION, DISCUSSION, HINT
+}
