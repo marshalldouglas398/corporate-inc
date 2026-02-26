b8bc564c45b801767d8fc368d0dc532e3455f4e1
edenmoore10 <edenrm@email.sc.edu>
2026-02-26 18:32:23 -0500

    #66 added javadoc

--- src/main/java/com/model/QuestionTag.java
+++ src/main/java/com/model/QuestionTag.java
@@ -1,5 +1,7 @@
 package com.model;
-
+/**
+ * Enum representing the different tags that can be used on a question type
+ */
 public enum QuestionTag {
     FIND_THE_ERROR, FILL_IN_THE_BLANK,
     SITUATION, WRITE_CODE, TRACE_OUTPUT,
