c2d66bd08597c9d30e4c98eb11e9eda41ef94d41
edenmoore10 <edenrm@email.sc.edu>
2026-02-27 13:43:37 -0500

    fixed #56

--- src/main/java/com/model/DataWriter.java
+++ src/main/java/com/model/DataWriter.java
@@ -1,6 +1,9 @@
 package com.model;
-
-
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 public class DataWriter {
     public static boolean saveUsers() {
         return true;
---
+++ src/main/java/com/model/UserList.java
@@ -0,0 +1,12 @@
+package com.model;
+
+import java.util.ArrayList;
+
+public class UserList {
+    private UserList userlist;
+    private ArrayList<User> users;
+
+    private UserList() {
+        this.users = new ArrayList<User>();
+    }
+}
