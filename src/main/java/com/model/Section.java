40463a2dc1a38c6f2f799e8f1cda6d799b5ac80b
marshalldouglas398 <marshalldouglas398@gmail.com>
2026-02-27 01:07:25 -0500

    Added getters and setters to Section.java

--- src/main/java/com/model/Section.java
+++ src/main/java/com/model/Section.java
@@ -24,4 +24,68 @@ public class Section {
         this.file = file;
         this.code = code;
     }
+
+    /**
+     * Gets the title of the section
+     * @return The title of the section
+     */
+    public String getTitle() {
+        return this.title;
+    }
+
+    /**
+     * Gets the description of the section
+     * @return The description of the section
+     */
+    public String getDescription() {
+        return this.description;
+    }
+
+    /**
+     * Gets the file associated with the section
+     * @return The file associated with the section
+     */
+    public File getFile() {
+        return this.file;
+    }
+
+    /**
+     * Gets the code associated with the section
+     * @return The code associated with the section
+     */
+    public String getCode() {
+        return this.code;
+    }
+
+    /**
+     * Sets the title of the section
+     * @param title The new title to set for the section
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * Sets the description of the section
+     * @param description The new description to set for the section
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Sets the file associated with the section
+     * @param file The new file to set for the section
+     */
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    /**
+     * Sets the code associated with the section
+     * @param code The new code to set for the section
+     */
+    public void setCode(String code) {
+        this.code = code;
+    }
 }
