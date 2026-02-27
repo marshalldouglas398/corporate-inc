4f815d7c3b03746a066ea97766b5cffd317e1819
Marcuss2106 <marcuss2106@gmail.com>
2026-02-27 14:14:30 -0500

    fixed #35

--- src/main/java/com/model/DataWriter.java
+++ src/main/java/com/model/DataWriter.java
@@ -1,16 +1,19 @@
 package com.model;
-
-
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 public class DataWriter {
     public static boolean saveUsers() {
         return true;
     }
 
     public static boolean saveQuestions() {
-        return false;
+        return true;
     }
 }
 
 public static void main(String[] args){
-    DataWriter.saveUsers();
+    
 }
---
+++ src/main/java/com/model/UserList.java
@@ -0,0 +1,49 @@
+package com.model;
+
+import java.util.ArrayList;
+
+public class UserList {
+    private static  UserList userlist;
+    private ArrayList<User> users;
+
+    private UserList() {
+        this.users = new ArrayList<User>();
+    }
+
+    public static UserList getInstance() {
+        if (userlist == null) {
+            userlist = new UserList();
+        }
+        return userlist;
+    }
+    public User searchUser(String username) {
+        for (User user : users) {
+            if (user.getUsername().equals(username)) {
+                return user;
+            }
+        }
+        return null;
+    }
+    public User getUser(String username, String password) {
+        for (User user : users) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                return user;
+            }
+        }
+        return null;
+    }
+    public boolean checkForUser(String password, String username) {
+        for (User user : users) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean save() { // to do
+        return true;
+    }
+    public boolean isAdmin(User user) {
+        return user.isAdmin();
+    }
+}
