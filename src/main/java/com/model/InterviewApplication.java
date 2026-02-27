8abe2f36565f448935d727535f0098a720925524
edenmoore10 <edenrm@email.sc.edu>
2026-02-27 14:29:05 -0500

    fixed #2

---
+++ src/main/java/com/model/InterviewApplication.java
@@ -0,0 +1,17 @@
+package com.model;
+/**
+ * This class represents an the actions you can take in the inteview application
+ * @Author Eden Moore
+ */
+public class InterviewApplication {
+    private QuestionList questionList;
+    private UserList userlist;
+    private User user;
+    private Question currentQuestion;
+
+    public InterviewApplication() {
+        this.questionList = QuestionList.getInstance();
+        this.userlist = UserList.getInstance();
+    }
+    
+}
