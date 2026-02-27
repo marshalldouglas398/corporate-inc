92598793303ad4d842ae7a77c91a163a9b53fdce
edenmoore10 <edenrm@email.sc.edu>
2026-02-27 13:47:51 -0500

    fixed #61

--- src/main/java/com/model/UserList.java
+++ src/main/java/com/model/UserList.java
@@ -3,10 +3,19 @@ package com.model;
 import java.util.ArrayList;
 
 public class UserList {
-    private UserList userlist;
+    private static  UserList userlist;
     private ArrayList<User> users;
 
     private UserList() {
         this.users = new ArrayList<User>();
     }
+
+    public static UserList getInstance() {
+        if (userlist == null) {
+            userlist = new UserList();
+        }
+        return userlist;
+    }
+    
+
 }
