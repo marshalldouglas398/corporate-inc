21baf71bbac4c0a4fdb954d06facec10c41e15cd
aweaver422 <aweaver422@gmail.com>
2026-03-02 09:20:10 -0500

    updated #152

--- src/main/java/com/model/Driver.java
+++ src/main/java/com/model/Driver.java
@@ -1,7 +1,61 @@
 package com.model;
 
 public class Driver {
+
+    private InterviewApplication interviewApp;
+
+    //is this the correct way to do it..?
+    void InterviewApplication() {
+        interviewApp = new InterviewApplication();
+    }
+
+    public void run() {
+        scenario1();
+        scenario2();
+    }
+
+    public void scenario1() {
+        System.out.println();
+        /*
+        if () {
+            System.out.println("Login failed");
+        } else {
+            System.out.println("Login successful");
+        }
+
+        System.out.println("[USER] is now logged in");
+        */
+        
+        //filter through questions
+
+        //select question
+
+        //add commentQ
+        
+        //rate question
+        
+        /*
+        if () {
+            System.out.println("Logout failed");
+        } else {
+            System.out.println("Logout successful");
+        }
+        */
+    }
+
+    public void scenario2() {
+        System.out.println();
+
+        //login
+
+        //add question
+
+        //add section
+
+        //logout
+    }
+
     public static void main(String[] args) {
-        System.out.println("test");
+        
     }
 }
