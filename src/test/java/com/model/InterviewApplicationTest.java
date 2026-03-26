c789bd5bd5d6a6744715fa5345b62aa50887e895
edenmoore10 <edenrm@email.sc.edu>
2026-03-26 12:49:25 -0400

    updated #176

--- src/test/java/com/model/InterviewApplicationTest.java
+++ src/test/java/com/model/InterviewApplicationTest.java
@@ -1,5 +1,167 @@
 package com.model;
+import org.junit.Test;
 
 public class InterviewApplicationTest {
+    // testing add question
+    @Test
+    public void adminAddsQuestion() {
+    }
+
+    @Test
+    public void editorAddsQuestion() {
+
+    }
+
+    @Test
+    public void studentAddsQuestion() {
+
+    }
+
+    @Test
+    public void editorAddsNullQuestion() {
+
+    }
+
+    @Test
+    public void editorAddsBlankQuestion() {
+
+    }
+
+    @Test
+    public void editorAddsExistingQuestion() {
+
+    }
+
+    @Test
+    public void editorAddsQuestionWithWrongEnum() {
+
+    }
+
+    // testing delete question
+
+    @Test
+    public void adminDeletesQuestion() {
+
+    }
+
+    @Test
+    public void editorDeletesQuestion() {
+
+    }
+
+    @Test
+    public void studentDeletesQuestion() {
+
+    }
+
+    @Test
+    public void editorDeletesQuestionWrongDate() {
+
+    }
+
+    // testing rate question
+
+    @Test
+    public void studentRatesQWithValidNumber() {
+
+    }
+
+    @Test
+    public void studentRatesQWithNegativeNumber() {
+
+    }
+
+    @Test
+    public void studentRatesQWithNumberGreatorThan5() {
+
+    }
+
+    @Test
+    public void studentRatesQuestionPreviouslyRated() {
+
+    }
+
+    //testing select question
+
+    @Test
+    public void studentSelectsQuestionThatExists() {
+
+    }
+
+    @Test
+    public void studentSelectsQuestionThatDoesntExist() {
+
+    }
+
+    @Test
+    public void studentSelectsCurrentQuestion() {
+
+    }
+
+    // testing rate comment
+
+    @Test
+    public void studentRatesCWithValidNumber() {
+
+    }
+
+    @Test
+    public void studentRatesCWithNegativeNumber() {
+
+    }
+
+    @Test
+    public void studentRatesCWithNumberGreatorThan5() {
+
+    }
     
+    @Test
+    public void studentRatesCommentPreviouslyRated() {
+
+    }
+
+    //testing toEditor
+
+    @Test
+    public void studentToEditor() {
+
+    }
+
+    @Test
+    public void editorToEditor() {
+
+    }
+
+    @Test
+    public void adminToEditor() {
+
+    }
+
+    @Test
+    public void nonexistingUserToEditor() {
+
+    }
+
+    // testing search questions
+
+    @Test
+    public void studentSearchingCorrectKeyword() {
+
+    }
+
+    @Test
+    public void studentSearchingKeywordNonExistant() {
+
+    }
+
+    @Test
+    public void studentSearchingMisspelledKeyword() {
+
+    }
+
+    @Test
+    public void studentSearchingKeywordWeirdCasing() {
+        
+    }
 }
+
