659359c61c844014346b97c37830ef0e06756fcd
edenmoore10 <edenrm@email.sc.edu>
2026-04-08 13:38:25 -0400

    updated #215

--- src/main/java/com/controllers/HomeController.java
+++ src/main/java/com/controllers/HomeController.java
@@ -5,9 +5,11 @@ import java.io.IOException;
 import com.corporate.App;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class HomeController {
-
+    @FXML
+    private Button primaryButton;
     @FXML
     private void goToLogin() throws IOException {
         App.setRoot("login");
