c05aa8e5821970ac9d88e65a3c36591a9f5be400
Marcuss2106 <marcuss2106@gmail.com>
2026-04-07 22:34:51 -0400

    fixed #220

--- src/main/java/com/corporate/App.java
+++ src/main/java/com/corporate/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("home"), 640, 480);
+        scene = new Scene(loadFXML("home"), 1100, 760);
         stage.setScene(scene);
         stage.show();
     }
