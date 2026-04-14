fd05e5e2dbb824070de7df4d002cd3072df86662
edenmoore10 <edenrm@email.sc.edu>
2026-04-13 22:23:14 -0400

    updated #226

--- src/main/java/com/controllers/DashController.java
+++ src/main/java/com/controllers/DashController.java
@@ -1,10 +1,19 @@
 package com.controllers;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.UUID;
 
 import com.corporate.App;
-import com.model.User;
+import com.model.Course;
+import com.model.Question;
+import com.model.QuestionList;
+import com.model.Student;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
@@ -126,21 +135,140 @@ public class DashController {
     @FXML
     private Text rct_date_2;
 
-    private User currentUser;
+    private Student currentUser;
 
 
-    public void setUser(User user) {
+    public void setUser(Student user) {
         this.currentUser = user;
         displayWelcome(user.getUsername());
+        // setting date
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE, MMMM, d, yyyy", Locale.ENGLISH);
+        String formattedDate = LocalDate.now().format(formatter);
+        setDate(formattedDate);
+        // setting account summary
+        int num_q = 0;
+        for(UUID q : currentUser.getQuestionsAnswered()) {
+            if(q != null) {
+                num_q++;
+            }
+        }
+        setNumQ(num_q);
+        ArrayList<Course> courses = new ArrayList<>();
+        courses.addAll(currentUser.getCoursesTaken());
+        setCourses(courses);
+        setMajor(currentUser.getMajor());
+        setStreak(currentUser.getStreak());
+
+        //recent activity
+        int recent = currentUser.getQuestionsAnswered().size() - 1;
+        if(recent == 1) {
+            QuestionList ql = QuestionList.getInstance();
+            Question q = ql.getQuestion(currentUser.getQuestionsAnswered().get(recent));
+            rct_1_title.setText(q.getTitle());
+            String type = q.getType().toString();
+            String noenumtype = type.substring(type.indexOf('.') + 1);
+            rct_1_type.setText(noenumtype);
+            String diff = q.getDifficulty().toString();
+            String noenumdiff = diff.substring(diff.indexOf('.') + 1);
+            rct_1_diff.setText(noenumdiff);
+            rct_date_1.setText("Date Completed"); // need to implement
+            rct_2_title.setVisible(false);
+            rct_2_type.setVisible(false);
+            rct_2_diff.setVisible(false);
+            rct_date_2.setVisible(false);
+        } else if(recent > 1) {
+            QuestionList ql = QuestionList.getInstance();
+            Question q = ql.getQuestion(currentUser.getQuestionsAnswered().get(recent));
+            Question q2 = ql.getQuestion(currentUser.getQuestionsAnswered().get(recent - 1));
+            rct_1_title.setText(q.getTitle());
+            String type = q.getType().toString();
+            String noenumtype = type.substring(type.indexOf('.') + 1);
+            rct_1_type.setText(noenumtype);
+            String diff = q.getDifficulty().toString();
+            String noenumdiff = diff.substring(diff.indexOf('.') + 1);
+            rct_1_diff.setText(noenumdiff);
+            rct_date_1.setText("Date Completed"); // need to implement
+            rct_2_title.setText(q2.getTitle());
+            String type2 = q2.getType().toString();
+            String noenumtype2 = type2.substring(type2.indexOf('.') + 1);
+            rct_2_type.setText(noenumtype2);
+            String diff2 = q2.getDifficulty().toString();
+            String noenumdiff2 = diff2.substring(diff2.indexOf('.') + 1);
+            rct_2_diff.setText(noenumdiff2);
+            rct_date_2.setText("Date Completed"); // need to implement
+        } else if(recent == 0) {
+            rct_1_title.setVisible(false);
+            rct_1_type.setVisible(false);
+            rct_1_diff.setVisible(false);
+            rct_date_1.setVisible(false); 
+            rct_2_title.setVisible(false);
+            rct_2_type.setVisible(false);
+            rct_2_diff.setVisible(false);
+            rct_date_2.setVisible(false);
+        }
+        // setting suggested
+        if(recent == 0 || recent < 3) { // maybe change numbers?
+
+        } else if(recent >= 3 || recent < 10) {
+
+        } else if(recent >= 10) {
+            
+        }
     }
-    
+
     @FXML
     private void logout() throws IOException {
         App.setRoot("login");
     }
 
     @FXML
+    private void goToQuestions(ActionEvent event) throws IOException {
+        //App.setRoot("questions"); not implemented yet
+    }
+
+    @FXML
+    private void goToDashboard(ActionEvent event) throws IOException {
+        App.setRoot("dash");
+    }
+
+    @FXML
+    private void goToSettings(ActionEvent event) throws IOException {
+        //App.setRoot("settings"); not implemented yet
+    }
+
+    @FXML
     public void displayWelcome(String username) {
         welcomeMessage.setText("Welcome, " + username + "!");
+    }
+
+    @FXML
+    public void setDate(String date) {
+        date_text.setText(date);
+    }
+
+    @FXML
+    public void setNumQ(int num) {
+        num_q_text.setText(Integer.toString(num));
+    }
+
+    @FXML
+    public void setCourses(ArrayList<Course> courses) {
+        String courseStr = "";
+        for(Course c : courses) {
+            String course = c.toString();
+            String noenum = course.substring(course.indexOf('.') + 1);
+            courseStr += noenum + "\n";
+        }
+        crs_text.setText(courseStr);
+    }
+
+    @FXML
+    public void setMajor(String major) {
+        mjr_text.setText(major);
+    }
+
+    @FXML
+    public void setStreak(int streak) {
+        stk_num_text.setText(Integer.toString(streak));
     }
 }
