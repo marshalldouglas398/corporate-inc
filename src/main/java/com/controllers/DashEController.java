88257f88a6629090542e9d79d4cea96ec09fb07d
edenmoore10 <edenrm@email.sc.edu>
2026-04-21 12:14:18 -0400

    fixed #228

--- src/main/java/com/controllers/DashEController.java
+++ src/main/java/com/controllers/DashEController.java
@@ -19,6 +19,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
 public class DashEController {
@@ -65,6 +66,9 @@ public class DashEController {
     private Text rct_1_diff;
 
     @FXML
+    private Rectangle diff_rect_1;
+
+    @FXML
     private Text rct_2_title;
 
     @FXML
@@ -72,6 +76,9 @@ public class DashEController {
 
     @FXML
     private Text rct_2_diff;
+    
+    @FXML
+    private Rectangle diff_rect_2;
 
     private Editor currentUser;
     private InterviewApplication app;
@@ -104,10 +111,21 @@ public class DashEController {
             rct_1_type.setText(noenumtype);
             String diff = q.getDifficulty().toString();
             String noenumdiff = diff.substring(diff.indexOf('.') + 1);
+            if(noenumdiff.equals("EASY")) {
+                diff_rect_1.setStyle("-fx-fill: #AFFFAF");
+                rct_1_diff.setStyle("-fx-fill: #487D48");
+            } else if(noenumdiff.equals("MEDIUM")) {
+                diff_rect_1.setStyle("-fx-fill: #ffc40066");
+                rct_1_diff.setStyle("-fx-fill: #AD7032");
+            } else if(noenumdiff.equals("HARD")) {
+                diff_rect_1.setStyle("-fx-fill: #FF000066");
+                rct_1_diff.setStyle("-fx-fill: #B41D2C");
+            }
             rct_1_diff.setText(noenumdiff);
             rct_2_title.setVisible(false);
             rct_2_type.setVisible(false);
             rct_2_diff.setVisible(false);
+            diff_rect_2.setVisible(false);
         } else if(recent > 1) {
             QuestionList ql = QuestionList.getInstance();
             Question q = ql.getQuestion(currentUser.getQuestionsMade().get(recent));
@@ -118,6 +136,16 @@ public class DashEController {
             rct_1_type.setText(noenumtype);
             String diff = q.getDifficulty().toString();
             String noenumdiff = diff.substring(diff.indexOf('.') + 1);
+            if(noenumdiff.equals("EASY")) {
+                diff_rect_1.setStyle("-fx-fill: #AFFFAF");
+                rct_1_diff.setStyle("-fx-fill: #487D48");
+            } else if(noenumdiff.equals("MEDIUM")) {
+                diff_rect_1.setStyle("-fx-fill: #ffc40066");
+                rct_1_diff.setStyle("-fx-fill: #AD7032");
+            } else if(noenumdiff.equals("HARD")) {
+                diff_rect_1.setStyle("-fx-fill: #FF000066");
+                rct_1_diff.setStyle("-fx-fill: #B41D2C");
+            }
             rct_1_diff.setText(noenumdiff);
             rct_2_title.setText(q2.getTitle());
             String type2 = q2.getType().toString();
@@ -125,14 +153,26 @@ public class DashEController {
             rct_2_type.setText(noenumtype2);
             String diff2 = q2.getDifficulty().toString();
             String noenumdiff2 = diff2.substring(diff2.indexOf('.') + 1);
+            if(noenumdiff2.equals("EASY")) {
+                diff_rect_2.setStyle("-fx-fill: #AFFFAF");
+                rct_2_diff.setStyle("-fx-fill: #487D48");
+            } else if(noenumdiff2.equals("MEDIUM")) {
+                diff_rect_2.setStyle("-fx-fill: #ffc40066");
+                rct_2_diff.setStyle("-fx-fill: #AD7032");
+            } else if(noenumdiff2.equals("HARD")) {
+                diff_rect_2.setStyle("-fx-fill: #FF000066");
+                rct_2_diff.setStyle("-fx-fill: #B41D2C");
+            }
             rct_2_diff.setText(noenumdiff2);
         } else if(recent == 0) {
             rct_1_title.setVisible(false);
             rct_1_type.setVisible(false);
             rct_1_diff.setVisible(false);
+            diff_rect_1.setVisible(false);
             rct_2_title.setVisible(false);
             rct_2_type.setVisible(false);
             rct_2_diff.setVisible(false);
+            diff_rect_2.setVisible(false);
         }
     }
 
